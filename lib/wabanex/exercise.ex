433ab3e0391d49b775a6cdbfdbe466c4fca648e1
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-08-26 20:57:56 -0300

    feat: add modules training and exercise

---
+++ lib/wabanex/exercise.ex
@@ -0,0 +1,29 @@
+defmodule Wabanex.Exercise do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Wabanex.Training
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
+  @fields [:name, :youtube_video_url, :protocol_description, :repetitions]
+
+  schema "exercises" do
+    field :name, :string
+    field :youtube_video_url, :string
+    field :protocol_description, :string
+    field :repetitions, :string
+
+    belongs_to :training, Training
+
+    timestamps()
+  end
+
+  def changeset(exercise, params) do
+    exercise
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+  end
+
+end
