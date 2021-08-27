b103813a925f282623fe0cb4246cc1cfa39dbcc4
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-08-27 16:26:29 -0300

    test: when a valid id is given, returns the user

---
+++ test/wabanex_web/schema_test.exs
@@ -0,0 +1,35 @@
+defmodule WabanexWeb.SchemaTest do
+  use WabanexWeb.ConnCase, async: true
+  alias Wabanex.User
+  alias Wabanex.Users.Create
+
+  describe "users queries" do
+    test "when a valid id is given, returns the user", %{conn: conn} do
+      params = %{name: "Joe Doe", email: "joe.doe@email.com", password: "123pass"}
+      {:ok, %User{id: user_id}} = Create.call(params)
+      query = """
+        {
+          user(id: "#{user_id}"){
+            id
+            name
+            email
+          }
+        }
+      """
+      response =
+        conn
+        |> post("/api/graphql", %{query: query})
+        |> json_response(:ok)
+        expected_response = %{
+          "data" => %{
+            "user" => %{
+              "email" => "joe.doe@email.com",
+              "id" => "#{user_id}",
+              "name" => "Joe Doe"
+            }
+          }
+        }
+      assert response == expected_response
+    end
+  end
+end
