f3b891493a6ef4d8e9e06331fb169efc26c6d980
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-08-26 21:47:17 -0300

    feat: add mutation trainings GraphQL

--- lib/wabanex_web/schema/types/root.ex
+++ lib/wabanex_web/schema/types/root.ex
@@ -2,9 +2,14 @@ defmodule WabanexWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
 
   alias Crudry.Middlewares.TranslateErrors
-  alias WabanexWeb.Resolvers.User, as: UserResolver
+  alias WabanexWeb.Resolvers
+  alias WabanexWeb.Schema.Types
+  alias Resolvers.User, as: UserResolver
+  alias Resolvers.Training, as: TrainingResolver
 
-  import_types WabanexWeb.Schema.Types.User
+  import_types Types.Custom.UUID4
+  import_types Types.User
+  import_types Types.Training
 
   object :root_query do
     field :user, type: :user do
@@ -17,6 +22,12 @@ defmodule WabanexWeb.Schema.Types.Root do
     field :user, type: :user do
       arg :input, non_null(:create_user_input)
       resolve &UserResolver.create/2
+      middleware TranslateErrors
+    end
+
+    field :training, type: :training do
+      arg :input, non_null(:create_training_input)
+      resolve &TrainingResolver.create/2
       middleware TranslateErrors
     end
   end
