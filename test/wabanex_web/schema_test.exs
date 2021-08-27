2fcf9c8d8493ca3fbd44a459e58155cfbf527516
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-08-27 16:37:20 -0300

    test: when all params are valid, creates the user

--- test/wabanex_web/schema_test.exs
+++ test/wabanex_web/schema_test.exs
@@ -32,4 +32,26 @@ defmodule WabanexWeb.SchemaTest do
       assert response == expected_response
     end
   end
+
+  describe "users mutations" do
+    test "when all params are valid, creates the user", %{conn: conn} do
+      mutation = """
+        mutation {
+          user(input: {
+            name: "Joe Doe",
+            email: "joe.doe@email.com",
+            password: "123pass"}
+          ){
+            id
+            name
+          }
+        }
+      """
+      response =
+        conn
+        |> post("/api/graphql", %{query: mutation})
+        |> json_response(:ok)
+      assert %{"data" => %{"user" => %{"id" => _id, "name" => "Joe Doe"}}} = response
+    end
+  end
 end
