5818dc237e42f681bef8311f4b14a07512e275a9
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-08-27 08:38:16 -0300

    test: when the file exists, returns the data

---
+++ test/wabanex/imc_test.exs
@@ -0,0 +1,20 @@
+defmodule Wabanex.IMCTest do
+  use ExUnit.Case, async: true
+
+  alias Wabanex.IMC
+
+  describe "calculate/1" do
+    test "when the file exists, returns the data" do
+      params = %{"filename" => "students.csv"}
+      response = IMC.calculate(params)
+      expected_response = {:ok, %{
+        "Anderson" => 27.64,
+        "Dani" => 23.44,
+        "Diego" => 23.04,
+        "Gabul" => 22.86,
+        "Rodrigo" => 26.23
+      }}
+      assert response == expected_response
+    end
+  end
+end
