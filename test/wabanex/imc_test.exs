e7ee83722329442cf327f165b5b1be9426cc3044
Anderson Santos <anderson.feitoza.santos@gmail.com>
2021-08-27 08:41:11 -0300

    test: when then wrong filename is given, returns an error

--- test/wabanex/imc_test.exs
+++ test/wabanex/imc_test.exs
@@ -16,5 +16,12 @@ defmodule Wabanex.IMCTest do
       }}
       assert response == expected_response
     end
+
+    test "when then wrong filename is given, returns an error" do
+      params = %{"filename" => "another.csv"}
+      response = IMC.calculate(params)
+      expected_response = {:error, "Error while opening the file"}
+      assert response == expected_response
+    end
   end
 end
